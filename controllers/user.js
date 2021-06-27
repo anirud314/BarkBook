dc62a7a2d63bd93d75f82baaea3fbcaac1e5d34b
nitinvemuri <80983324+nitinvemuri@users.noreply.github.com>
2021-06-27 16:42:12 -0700

    logout

--- controllers/user.js
+++ controllers/user.js
@@ -42,7 +42,39 @@ router.post('/login', (req, res) => {
             res.status(400).json({ message:'User not found'})
             return;
         }
+    });
+});
+
+router.put('./:id', (req,res) => {
+    User.update(req.body, {
+        individualHooks: true,
+        where: {
+            id: req.params.id,
+        }
     })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(404).jsonp('lalalalal')
+            return;
+        }
+        res.json(dbUserData);
+    });
+});
+
+router.delete('./:id', (req, res) => {
+    User.destroy(req.body, {
+        where: {
+            id: req.params.id
+        }
+    }).then(dbUserData => {
+        if (!dbUserData) {
+            res.status(204).json({ message: 'Not found'})
+        };
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json(err);
+    });
 })
 
 module.exports = router;
