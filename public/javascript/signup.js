5f0b85519db48d543c96592bd0b3abf00235b226
augustinandrea <augustin.andrea@gmail.com>
2021-06-27 19:14:28 -0700

    Initial Javascript
    The initial javascript needed.

---
+++ public/javascript/signup.js
@@ -0,0 +1,28 @@
+async function signupFormHandler(event) {
+    event.preventDefault();
+  
+    const username = document.querySelector('#username-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+  
+    if (username && password) {
+      const response = await fetch('/api/users', {
+        method: 'post',
+        body: JSON.stringify({
+          username,
+          password
+        }),
+        headers: { 'Content-Type': 'application/json' }
+      });
+  
+      // check the response status
+      if (response.ok) {
+        console.log('success');
+        document.location.replace('/dashboard');
+      } else {
+        alert(response.statusText);
+      }
+    }
+}
+
+// attaching the function to a signup button  
+document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
