56cb540dead5fc369beebf9e8f4f1e07a18140af
nitinvemuri <80983324+nitinvemuri@users.noreply.github.com>
2021-06-27 19:37:43 -0700

    login and create user

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -15,32 +15,116 @@ router.get('/', (req, res) => {
   });
 });
 
-router.post('/', (req,res) => {
-  User.create({
-    username: req.body.username,
-    password: req.body.password,
-    email: req.body.email,
-  }).then(dbUserData => {
-    req.session.user_id = dbUserData.id;
-    req.session.username = dbUserData.username || dbUserData.email;
-    req.session.loggedIn = true;
-    res.status(200).jsonp('new user created')
+router.get('/:id',  (req, res) => {
+  User.findByPk(req.params.id, {
+    attributes: { exclude: ['password'] }
   })
-})
+  .then(user => res.status(200).send(user))
+  .catch(err => {
+    console.log(err);
+    res.status(500).send({error: err.message});
+  });
+});
 
-router.post('/user/login', (req,res) => {
-  if (!req.body.username) {
-    err.push('Username not found')
-    return;
-  } else if (!req.body.password) {
-    err.push('Password not found')
-    return;
-  } else if (username === null) {
-    err.push('input a username')
-    return;
-  } else if (password === null) {
-    err.push('input a password')
+//Create a user
+router.post('/', (req,res) => {
+  //error checking
+  const errors = [];
+
+  if (!req.body.username){
+    error.push('Missing username');
   }
-  
-})
+
+  if (!req.body.email){
+    errors.push('Missing email address');
+  }
+
+  if (!req.body.password){
+    errors.push('Missing password');
+  }
+
+  if (errors.length > 0){
+    res.status(500).send({error: errors});
+    return;
+  }
+
+
+  User.create(req.body)
+  .then(newUser => {
+    req.session.save(() => {
+      req.session.user_id = newUser.id;
+      req.session.username = newUser.username;
+      req.session.email = newUser.email;
+      req.session.loggedIn = true;
+      newUser.password = ''; //clear the password
+      res.status(200).send(newUser);
+    })
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).send({error: err.message});
+  });
+});
+
+//login
+router.post('/login', (req, res) => {
+  //error checking
+  const errors = [];
+  if (!req.body.email){
+    errors.push('Missing email address');
+  }
+
+  if (!req.body.password){
+    errors.push('Missing password');
+  }
+
+  if (errors.length > 0){
+    res.status(500).send({error: errors});
+    return;
+  }
+
+  router.post('/login', (req,res) => {
+    if (!req.body.username) {
+      err.push('Username not found')
+      return;
+    } else if (!req.body.password) {
+      err.push('Password not found')
+      return;
+    } else if (username === null) {
+      err.push('input a username')
+      return;
+    } else if (password === null) {
+      err.push('input a password')
+    }
+    
+  })
+
+
+
+  User.findByPk(req.body.email)
+  .then(foundUser => {
+    if (!foundUser){
+      res.status(400).send({error: 'User not found'});
+      return;
+    }
+
+    const validPassword = foundUser.checkPassword(req.body.password);
+
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).send({error: err.message});
+  });
+});
+
+router.post('/logout', (req,res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
 module.exports = router;
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,13 +4,24 @@ const sequelize = require('../config/connection');
 // get all posts for homepage
 router.get('/', (req, res) => {
   console.log('Breath... Encourage your teammates... Breath... Repeat...');
+  const posts = [
+    {body: "Hello World", vote_count: 24, image: "/", comments: [{comment:"This is bob's commentif soifu sofius odifuos ufosiufoasu foasiuf osuifosiu fosufoasufasdoifuosdu fosiuf osudfo sudfoisufos ufosuifosu fosiuf", user: {username: "Bob"}}], user: {username:"Jeffs_Post"}}
+  ]
   
-  res.render('homepage', {});
+  res.render('homepage',{posts, layout: 'main'});
 });
 
-router.get('/login', (req,res) => {
+router.get('/login', (req, res) => {
   res.render('login');
 });
 
+router.get('/new-account',(req,res) => {
+  res.render('newaccount');
+})
+
 
 module.exports = router;
+
+
+
+//added to render comments 6-25
