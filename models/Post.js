403b88f20fc48dc3262764a451845536135e218d
nitinvemuri <80983324+nitinvemuri@users.noreply.github.com>
2021-06-28 19:46:50 -0700

    post-url

--- models/Post.js
+++ models/Post.js
@@ -49,13 +49,7 @@ Post.init(
       type: DataTypes.STRING,
       allowNull: false
     },
-    post_url: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        isURL: true
-      }
-    },
+
     user_id: {
       type: DataTypes.INTEGER,
       references: {
