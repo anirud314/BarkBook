46764ba6bd2f1e7b909d9412fab8dd98c4c25c6e
Aniruddha Bhattacharya <p.aniruddhabhattacharya@gmail.com>
2021-06-28 19:52:40 -0700

    added post seed

--- seeds/index.js
+++ seeds/index.js
@@ -1,11 +1,14 @@
 const seedUsers = require('./user-seeds');
 
 const sequelize = require('../config/connection');
+const seedPosts = require('./post-seeds');
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
   console.log('--------------');
   await seedUsers();
+  console.log('--------------');
+  await seedPosts();
 
   process.exit(0);
 };
---
+++ seeds/post-seeds.js
@@ -0,0 +1,22 @@
+const sequelize = require('../config/connection');
+const {Post} = require('../models');
+
+const postdata = [
+    {
+       title: 'Gud Doggo',
+       user_id: 1
+       
+    },
+    {
+        title: 'bad Doggo',
+        user_id: 2
+        
+    },
+    {
+        title: 'Gud Doggo',
+        user_id: 3
+    }
+];
+const seedPosts = () => Post.bulkCreate(postdata, {individualHooks: true});
+
+module.exports = seedPosts;
